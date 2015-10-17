b9070f8abc596a73e64b14a211e254a6133e0185
Johan Pettersson <xsnudelx@hotmail.com>
2015-10-17 21:54:16 +0200

    init

--- SimpleCommunityMessager/Controllers/HomeController.cs
+++ SimpleCommunityMessager/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SimpleCommunityMessager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +8,36 @@ using System.Web.Mvc;
 
 namespace SimpleCommunityMessager.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+
+            var CurrentUser = db.Users.Find(User.Identity.GetUserId());
+
+            var posts = from p in db.Posts
+                        select p;
+
+            posts = posts.Where(p => p.Id.Equals(CurrentUser));
+
+            posts = posts.Where(p => p.Read.Equals(false));
+
+            IndexDTO dto = new IndexDTO();
+
+            //dto.unreadCounter = posts.Count();
+
+            dto.unreadCounter = 2;
+
+            dto.userName = CurrentUser.UserName;
+
+            dto.lastLogin = CurrentUser.LastLogin;
+
+            dto.loginCounter = CurrentUser.LoginCounter;
+
+            return View(dto);
         }
 
         public ActionResult About()
---
+++ SimpleCommunityMessager/Models/IndexDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCommunityMessager.Models
+{
+    public class IndexDTO
+    {
+        public int unreadCounter { get; set; }
+        public string userName { get; set; }
+        public DateTime lastLogin { get; set; }
+        public int loginCounter { get; set; }
+    }
+}
