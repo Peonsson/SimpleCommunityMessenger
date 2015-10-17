df1aa1931321cc1ace7d4a6a31f8ee2097e1c259
Johan Pettersson <xsnudelx@hotmail.com>
2015-10-17 18:12:56 +0200

    init

--- SimpleCommunityMessager/Models/Post.cs
+++ SimpleCommunityMessager/Models/Post.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace SimpleCommunityMessager.Models
 {
     public class Post
     {
-        public int PostID { get; set; }
+        public int ID { get; set; }
+        public String Subject { get; set; }
+        public String Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool Read { get; set; }
+        public bool Deleted { get; set; }
+        public ApplicationUser Sender { get; set; }
+        public ApplicationUser Receiver { get; set; }
+    }
+
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
     }
 }
