af2924950488c352c9056cf98c82064aeec75e54
roppe546 <roppe546@gmail.com>
2015-10-17 17:23:55 +0200

    Added initial post class.

---
+++ SimpleCommunityMessager/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCommunityMessager.Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+    }
+}
